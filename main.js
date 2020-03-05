03a99ec413ce4fdfb1f87a73b76021efe16d45c5
songxw <songxw17@qq.com>
2020-03-05 15:50:01 +0800

    “修改循环次数”

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ var FIbonacci = {
 }
 Fibonacci = Object.create(FIbonacci)
 var main = function () {
-    num = 100;
-    for (var i = 1; i <= 200; i++) {
+    num = 200;
+    for (var i = 1; i <= num; i++) {
         console.log("斐波那契额数列第" + i + "是：" + Fibonacci.of(i))
     }
 }
